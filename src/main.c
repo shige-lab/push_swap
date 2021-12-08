f3713e97e21a7d64a675da09c5fa2c51812f923a
shige <shigebask@gmail.com>
2021-12-08 20:56:14 +0900

    fix: astackが最初からソートされてた場合はreturn0

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tshigena <tshigena@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 00:47:23 by tshigena          #+#    #+#             */
-/*   Updated: 2021/12/07 11:00:14 by tshigena         ###   ########.fr       */
+/*   Updated: 2021/12/08 20:53:49 by tshigena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int argc, char **argv)
 	data.total_size = argc - 1;
 	data.a.size = data.total_size;
 	agrv_to_a_stack(&data, argv);
+	if (diff(data, 0) == 0)
+		return (0);
 	push_swap(&data);
 	ft_free_stacks(data);
 	return (0);
